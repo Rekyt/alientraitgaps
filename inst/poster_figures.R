341e44388416edd092a4ef2edd972b98b4c08d53
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2022-11-09 15:08:56 +0100

    Resave trait proportion maps for poster

--- inst/poster_figures.R
+++ inst/poster_figures.R
@@ -14,6 +14,83 @@ ggsave(
 )
 
 
+# Maps
+
+tar_load(
+  c(regions_trait_prop, glonaf_small_islands, glonaf_mainland_large_islands)
+)
+
+
+# Background map
+world_sf = rnaturalearth::ne_countries(returnclass = "sf") %>%
+  sf::st_transform(crs = "+proj=eqearth")
+
+# Pivot trait data to be usable across facets
+pivoted_data = regions_trait_prop %>%
+  select(OBJIDsic, prop_with_any_trait:has_bergmann_prop) %>%
+  tidyr::pivot_longer(
+    !OBJIDsic, names_to = "prop_name", values_to = "prop_value"
+  ) %>%
+  mutate(
+    prop_name = factor(
+      prop_name,
+      levels = c("prop_with_any_trait", "has_lhs_prop", "has_diaz_prop",
+                 "has_bergmann_prop")
+    )
+  )
+
+# Mainland regions
+mainland_pivot = glonaf_mainland_large_islands %>%
+  inner_join(pivoted_data, by = "OBJIDsic")
+
+# Island regions
+island_pivot = glonaf_small_islands %>%
+  inner_join(pivoted_data, by = "OBJIDsic")
+
+# Clean environment
+rm(pivoted_data, glonaf_mainland_large_islands, glonaf_small_islands,
+   regions_trait_prop)
+
+# Actual plot
+actual_map = mainland_pivot %>%
+  ggplot(aes(fill = prop_value)) +
+  geom_sf(data = world_sf, fill = "gray85", color = "gray65", size = 1/100) +
+  # Non-small islands and mainlands
+  geom_sf(color = NA, size = 1/100) +
+  # Small islands
+  geom_sf(
+    aes(color = prop_value),
+    fill = NA,
+    data = island_pivot,
+    size = 1.2, shape = 21, stroke = 0.4
+  ) +
+  facet_wrap(
+    vars(prop_name),
+    labeller = labeller(
+      prop_name = c(
+        has_bergmann_prop = "Root Traits",
+        has_diaz_prop     = "Aboveground Traits",
+        has_lhs_prop      = "LHS Traits",
+        prop_with_any_trait = "Any trait"
+      )
+    )
+  ) +
+  scale_fill_viridis_b(
+    name = "Proportion of alien species\nwith trait combination",
+    labels = scales::percent_format(), n.breaks = 6, show.limits = TRUE
+  ) +
+  scale_color_viridis_b(
+    name = "Proportion of alien species\nwith trait combination",
+    labels = scales::percent_format(), n.breaks = 6, show.limits = TRUE
+  ) +
+  ylim(-5747986, NA) +  # Remove whatever is below 60°S
+  theme_void() +
+  theme(
+    legend.position = "top",
+    legend.key.width = unit(2, "lines"),
+    strip.background = element_blank(),
+    plot.margin      = margin(b = 3/11)
+  )
 
 ggsave(
   "../poster_alientraitgaps_sfe_metz_2022/fig_map_alien_richness.svg",
