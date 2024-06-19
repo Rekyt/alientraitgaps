f1ad013cfb638e6764b426e3b19205ac9f5066ea
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2024-06-19 20:44:26 +0200

    Update sGubik extraction routine

--- inst/sgubik_extract.R
+++ inst/sgubik_extract.R
@@ -49,34 +49,96 @@ austraits_simple = austraits_traits |>
   distinct(taxon_name, trait_name) |>
   inner_join(
     potential_traits |>
-      distinct(consolidated_name, austraits_trait_name)
+      distinct(consolidated_name, austraits_trait_name),
     by = c(trait_name = "austraits_trait_name")
   )
 bien_simple = bien_traits |>
   distinct(scrubbed_species_binomial, trait_name) |>
+  inner_join(
+    potential_traits |>
+      distinct(consolidated_name, bien_trait_name),
+    by = c(trait_name = "bien_trait_name")
+  )
+
+gift_simple = gift_traits |>
+  distinct(trait_ID, work_species) |>
+    inner_join(
+      tar_read(gift_trait_meta) |>
+        distinct(trait_ID = Lvl3, gift_trait_name = Trait2)
+    ) |>
+  inner_join(
+    potential_traits |>
+      distinct(consolidated_name, gift_trait_name),
+    by = "gift_trait_name"
+  )
+
+try_simple = try_traits |>
+  distinct(AccSpeciesName, TraitID) |>
+  inner_join(
+    potential_traits |>
+      distinct(consolidated_name, TraitID = as.integer(try_trait_id)),
+    by = "TraitID"
+  )
+
+total_traits = list(
+  austraits = austraits_simple |>
+    distinct(species = taxon_name, consolidated_name),
+  bien = bien_simple |>
+    distinct(species = scrubbed_species_binomial, consolidated_name),
+  gift = gift_simple |>
+    distinct(species = work_species, consolidated_name),
+  try = try_simple |>
+    distinct(species = AccSpeciesName, consolidated_name)
+) |>
+  bind_rows(.id = "database")
+
+total_diaz = total_traits |>
+  group_by(species) |>
+  summarise(traits = list(unique(consolidated_name))) |>
+  rowwise() |>
+  mutate(
+    has_diaz = ifelse(
+      any(apply(exact_combs$diaz, 1, \(x) all(x %in% traits))), TRUE, FALSE
+    )
+  ) |>
+    ungroup()
+
+with_diaz = total_diaz |>
+  filter(has_diaz)
+
+# Filter species with full GSPFF -----------------------------------------------
+
+austraits_gspff = austraits_traits |>
+  filter(taxon_name %in% with_diaz$species)
+
+bien_gspff = bien_traits |>
+  filter(scrubbed_species_binomial %in% with_diaz$species)
+
+gift_gspff = gift_traits |>
+  filter(work_species %in% with_diaz$species)
+
+try_gspff = try_traits |>
+  filter(AccSpeciesName %in% with_diaz$species)
 
 # Save all these data ----------------------------------------------------------
-if (dir.exists(here::here("inst", "data_extracts", "weihan_data"))) {
 
-  dir.create(here::here("inst", "data_extracts", "weihan_data"),
-             recursive = TRUE)
-}
+folder = here::here("inst", "data_extracts", "sgubik_extract")
 
 saveRDS(
-  austraits_traits,
-  here::here("inst", "data_extracts", "weihan_data", "austraits_weihan.Rds")
+  austraits_gspff,
+  here::here(folder, "austraits_gspff.Rds")
 )
 saveRDS(
-  bien_traits,
-  here::here("inst", "data_extracts", "weihan_data", "bien_weihan.Rds")
+  bien_gspff,
+  here::here(folder, "bien_gspff.Rds")
 )
 saveRDS(
-  gift_traits,
-  here::here("inst", "data_extracts", "weihan_data", "gift_weihan.Rds")
+  gift_gspff,
+  here::here(folder, "gift_gspff.Rds")
 )
 saveRDS(
-  try_traits,
-  here::here("inst", "data_extracts", "weihan_data", "try_weihan.Rds")
+  try_gspff,
+  here::here(folder, "try_gspff.Rds")
 )
 
 tictoc::toc()
