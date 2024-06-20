6cb1f8bddc041fc574042fa3f036bb47a560585d
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2024-06-20 11:59:24 +0200

    Added script to extract data for Anna

---
+++ inst/anna_extract.R
@@ -0,0 +1,36 @@
+# Script to extract traits for all of Anna's species
+
+# Packages ---------------------------------------------------------------------
+library("dplyr")
+library("targets")
+
+# Load initial data ------------------------------------------------------------
+folder = here::here("inst", "data_extracts", "anna_extract")
+
+host_species = readRDS(here::here(folder, "host_species_names.rds"))
+
+annas_species = unique(host_species$Taxon)
+
+# Get trait data ---------------------------------------------------------------
+
+austraits_traits = tar_read(austraits)$traits |>
+  filter(taxon_name %in% annas_species)
+
+bien_traits = tar_read(bien_traits) |>
+  filter(scrubbed_species_binomial %in% annas_species)
+
+gift_traits = tar_read(gift_raw_traits) |>
+  inner_join(
+    tar_read(gift_trait_meta) |>
+      distinct(trait_ID = Lvl3, trait_name = Trait2),
+    by = "trait_ID"
+  ) |>
+    filter(work_species %in% annas_species)
+
+try_traits = tar_read(full_try_df) |>
+  semi_join(
+    tar_read(try_harmonized_species) |>
+      filter(MatchedName %in% annas_species) |>
+        rename(AccSpeciesID = TRY_SpeciesID),
+    by = "AccSpeciesID"
+  )
